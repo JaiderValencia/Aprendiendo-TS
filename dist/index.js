3d6c9c3b57abded92a01192dcd0202ad3550b491
JaiderValencia <janvame05@gmail.com>
2023-04-23 23:15:43 -0500

    nuevas anotaciones

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,16 @@
 "use strict";
-let mensaje = "hola mundo";
-mensaje = "chanchito feliz";
+let mensaje = 'hola mundo';
+mensaje = 'chanchito feliz';
 console.log(mensaje);
-mensaje = "chao mundo";
+mensaje = 'chao mundo';
+let extincionDinos = 76_000_000;
+let dinoFav = 'Velociraptor';
+let extintos = true;
+let miVariable;
+miVariable = 'chanchito feliz';
+miVariable = 42;
+let animales = ['perro', 'koala', 'oso'];
+let numeros = [1, 2, 3, 4, 5];
+let checks = [true, false, true, true];
+let numeros2 = [];
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 //primera prueba
-let mensaje: string = "hola mundo";
+let mensaje: string = 'hola mundo';
 
-mensaje = "chanchito feliz";
+mensaje = 'chanchito feliz';
 
 console.log(mensaje);
 
-mensaje = "chao mundo"
+mensaje = 'chao mundo'
 
 //tipos
 
@@ -32,7 +32,7 @@ mensaje = "chao mundo"
 
 // * Tipos inferidos
 let extincionDinos = 76_000_000 // se sabe que es number por que se inicializó con un dato number
-let dinoFav: string = "Velociraptor"
+let dinoFav: string = 'Velociraptor'
 let extintos = true // se sabe que es boolean por que se inicializó con un boolean
 
 
@@ -40,6 +40,13 @@ let extintos = true // se sabe que es boolean por que se inicializó con un bool
 
 let miVariable // no se sabe que tipo de dato es por que no está inicializado, lo cual lo hace una variable dinamica
 
-miVariable = "chanchito feliz"
+miVariable = 'chanchito feliz'
 
 miVariable = 42
+
+let animales: string[] = ['perro', 'koala', 'oso'] // array de strings
+let numeros: number[] = [1, 2, 3, 4, 5] // array de numeros
+let checks: boolean[] = [true, false, true, true] // array de booleanos
+let numeros2: Array<number> = [] // otra forma de hacer arrays
+
+// animales.map(x=>x.) el autocompletado sugiere métodos del tipo de dato correspondiente
