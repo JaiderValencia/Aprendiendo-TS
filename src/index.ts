16775eaae61535369623096570b6874554a46010
JaiderValencia <janvame05@gmail.com>
2023-04-23 23:03:03 -0500

    agrego anotaciones

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 //primera prueba
-let mensaje:string = "hola mundo";
+let mensaje: string = "hola mundo";
 
 mensaje = "chanchito feliz";
 
@@ -7,4 +7,39 @@ console.log(mensaje);
 
 mensaje = "chao mundo"
 
-//tipo
+//tipos
+
+/*
+    * Tipos de JS:
+    * Number
+    * String
+    * Null
+    * Undefined
+    * Boolean
+    * Object
+    * Function 
+
+    * Tipos de TS:
+    * Any // No usarlo
+    * Unknow
+    * Never
+    * Arrays
+    * Tuplas
+    * Enums
+    
+    
+*/
+
+// * Tipos inferidos
+let extincionDinos = 76_000_000 // se sabe que es number por que se inicializó con un dato number
+let dinoFav: string = "Velociraptor"
+let extintos = true // se sabe que es boolean por que se inicializó con un boolean
+
+
+// No usar Any porque funcionaría como dinámico y la finaldad de TS es que sea estático. Siempre asignar tipo de dato
+
+let miVariable // no se sabe que tipo de dato es por que no está inicializado, lo cual lo hace una variable dinamica
+
+miVariable = "chanchito feliz"
+
+miVariable = 42
